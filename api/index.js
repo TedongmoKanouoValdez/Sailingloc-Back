f215dcb9423072386e82882718c14975fdaf9304
ArmadaHannibal <ynkoumate@gmail.com>
2025-08-17 21:25:49 +0200

    Add src/lib/prisma.js v9

--- api/index.js
+++ api/index.js
@@ -4,3 +4,9 @@ const app = require("../src/app");
 module.exports = (req, res) => {
   app(req, res);
 };
+
+// const PORT = process.env.PORT || 3001;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
