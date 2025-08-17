4e79fc8c624d93d6b546de102520aa02b60e9fc8
ArmadaHannibal <ynkoumate@gmail.com>
2025-08-17 12:40:30 +0200

    Add src/lib/prisma.js v5

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,19 +1,12 @@
+// lib/prisma.js
 const { PrismaClient } = require("@prisma/client");
 
 let prisma;
 
-if (process.env.NODE_ENV === "production") {
-  // En production, crée un client global pour Vercel serverless
-  if (!globalThis.prisma) {
-    globalThis.prisma = new PrismaClient();
-  }
-  prisma = globalThis.prisma;
-} else {
-  // En dev, on garde un singleton pour éviter plusieurs connexions
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+if (!global.prisma) {
+  global.prisma = new PrismaClient();
 }
+
+prisma = global.prisma;
 
 module.exports = prisma;
